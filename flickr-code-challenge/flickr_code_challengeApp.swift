96696d371d23ffb23a7099f87b604c7d1d59e8c0
preethamreddy298 <86038444+preethamreddy298@users.noreply.github.com>
2024-11-19 12:47:02 -0500

    Add files via upload

---
+++ flickr-code-challenge/flickr_code_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  flickr_code_challengeApp.swift
+//  flickr-code-challenge
+//
+//  Created by saipreetham on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct flickr_code_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
